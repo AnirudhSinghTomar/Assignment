c71f2144ce956c98b6d9bac9dfc5ac46cfdfd750
AnirudhSinghTomar <65367836+AnirudhSinghTomar@users.noreply.github.com>
2021-06-30 13:20:40 +0530

    Update script.js

--- LEVEL 3/script.js
+++ LEVEL 3/script.js
@@ -2,10 +2,9 @@ let arr = ["p", "button", "li"];
 let text = ["Hey this is Anirudh Singh Tomar", "Click to Know me", "By"];
 for (let i = 0; i < arr.length; i++) {
     var node = document.createElement(arr[i]);
-    var textnode = document.createTextNode(text[i]); // Create a text node
+    var textnode = document.createTextNode(text[i]); 
     node.appendChild(textnode);
     var element = document.getElementById("body");
     element.appendChild(node);
-    console.log("Added a new node", arr[i]); // Append the text to <li>
-
+    console.log("Added a new node", arr[i]); 
 }

f27012a5704d3b548aa0862b844f4ac9b211fa9e
AnirudhSinghTomar <65367836+AnirudhSinghTomar@users.noreply.github.com>
2021-07-01 12:55:11 +0530

    Update script.js

--- LEVEL 3/script.js
+++ LEVEL 3/script.js
@@ -1,10 +1,44 @@
-let arr = ["p", "button", "li"];
-let text = ["Hey this is Anirudh Singh Tomar", "Click to Know me", "By"];
-for (let i = 0; i < arr.length; i++) {
-    var node = document.createElement(arr[i]);
-    var textnode = document.createTextNode(text[i]); 
-    node.appendChild(textnode);
-    var element = document.getElementById("body");
-    element.appendChild(node);
-    console.log("Added a new node", arr[i]); 
-}
+
+const arr = ["p", "button", "li"];
+const text = ["Hey this is Anirudh Singh Tomar", "Click to Know me", "By"];
+
+let observer;
+        
+        document.addEventListener('DOMContentLoaded', init);
+        
+        function init(){
+            for (let i = 0; i < arr.length; i++) {
+   
+                var node = document.createElement(arr[i]);
+                var textnode = document.createTextNode(text[i]); 
+                node.appendChild(textnode);
+                var element = document.getElementById("body");
+                element.appendChild(node);
+            }
+            
+            let config = {
+                
+                childList: true, 
+                
+            };
+        
+            
+            observer = new MutationObserver(mutated);
+            observer.observe(document.body, config);
+        }
+        
+        
+        
+       
+        
+        
+        function mutated(mutationList){
+            console.log( mutationList );
+            for(let mutation of mutationList) {
+                if (mutation.type == 'childList') {
+                    console.log('Added a new node');
+                }
+                
+            }
+        }
+

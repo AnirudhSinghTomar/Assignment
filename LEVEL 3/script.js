03605e53df0926f9231f73bdf1486a66b0130d45
AnirudhSinghTomar <65367836+AnirudhSinghTomar@users.noreply.github.com>
2021-07-01 14:45:50 +0530

    Update script.js

--- LEVEL 3/script.js
+++ LEVEL 3/script.js
@@ -1,44 +1,48 @@
 const arr = ["p", "button", "button"];
 const text = ["Hey this is Anirudh Singh Tomar", "Click to Know me", "By"];
-function addtagss(){
-  let observer;
+
+let observer;
         
-  if(typeof MutationObserver !== "undefined"){
-      observer = new MutationObserver(mutated);
-      let config = {
+        document.addEventListener('DOMContentLoaded', init);
+        
+        function init(){
+            let p = document.querySelector('body'); 
+            p.addEventListener('click', change);
+            
+            let config = {
                 
-          childList: true, 
+                childList: true, 
+               
+            };
+          
+            
+            observer = new MutationObserver(mutated);
+            observer.observe(p, config);
+        }
         
-      };
-     observer.observe(document.body, config);
-  }else{
-     document.addEventListener('DOMContentLoaded', init);
-  }       
-       
-  for (let i = 0; i < arr.length; i++) {
-   
+        
+        
+        function change(ev){
+            
+            for (let i = 0; i < arr.length; i++) {
+                // let p = ev.currentTarget;
                 var node = document.createElement(arr[i]);
                 var textnode = document.createTextNode(text[i]); 
                 node.appendChild(textnode);
                 var element = document.getElementById("body");
                 element.appendChild(node);
             }
- }          
-                
+        }
         
-function inti(e){
-               console.log("Added a new node",e.target.tagName)
+        
+        
+        
+        function mutated(mutationList){
+            Array.prototype.forEach.call(mutationList,function (data){
+                let newnodes = data.addedNodes;
+                Array.prototype.forEach.call(newnodes,function (newN){
+                    console.log("Added a new node",newN.tagName);
+                } )
+            })
 }
-       
-        
-        
-function mutated(mutationList){
-               console.log( mutationList );
-               for(let mutation of mutationList) {
-                  if (mutation.type == 'childList') {
-                     console.log('Added a new node');
-                  }
-                
-                }
-}
-        
+  

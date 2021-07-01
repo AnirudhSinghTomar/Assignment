ed4e692fc19d7ec35282de255d10b1272db5ee13
AnirudhSinghTomar <65367836+AnirudhSinghTomar@users.noreply.github.com>
2021-07-01 14:53:44 +0530

    Update script.js

--- LEVEL 3/script.js
+++ LEVEL 3/script.js
@@ -2,8 +2,6 @@
  const text = ["Hey this is Anirudh Singh Tomar", "Click to Know me", "By"];
 function addtagss(){
   let observer;
-        
-  if(typeof MutationObserver !== "undefined"){
       observer = new MutationObserver(mutated);
       let config = {
                 
@@ -11,11 +9,8 @@ function addtagss(){
         
       };
      observer.observe(document.body, config);
-  }else{
-     document.addEventListener('DOMContentLoaded', init);
-  }       
-       
-  for (let i = 0; i < arr.length; i++) {
+    
+     for (let i = 0; i < arr.length; i++) {
    
                 var node = document.createElement(arr[i]);
                 var textnode = document.createTextNode(text[i]); 
@@ -23,15 +18,7 @@ function addtagss(){
                 var element = document.getElementById("body");
                 element.appendChild(node);
   }
- }          
-                
-        
-function inti(e){
-               console.log("Added a new node",e.target.tagName)
-}
-       
-        
-        
+ }                 
 function mutated(mutationList){
                Array.prototype.forEach.call(mutationList,function (data){
                    let newnodes = data.addedNodes;
@@ -41,51 +28,3 @@ function mutated(mutationList){
                })
 }
         
-// const arr = ["p", "button", "button"];
-// const text = ["Hey this is Anirudh Singh Tomar", "Click to Know me", "By"];
-
-// let observer;
-        
-//         document.addEventListener('DOMContentLoaded', init);
-        
-//         function init(){
-//             let p = document.querySelector('body'); 
-//             p.addEventListener('click', change);
-            
-//             let config = {
-                
-//                 childList: true, 
-               
-//             };
-          
-            
-//             observer = new MutationObserver(mutated);
-//             observer.observe(p, config);
-//         }
-        
-        
-        
-//         function change(ev){
-            
-//             for (let i = 0; i < arr.length; i++) {
-//                 // let p = ev.currentTarget;
-//                 var node = document.createElement(arr[i]);
-//                 var textnode = document.createTextNode(text[i]); 
-//                 node.appendChild(textnode);
-//                 var element = document.getElementById("body");
-//                 element.appendChild(node);
-//             }
-//         }
-        
-        
-        
-        
-//         function mutated(mutationList){
-//             Array.prototype.forEach.call(mutationList,function (data){
-//                 let newnodes = data.addedNodes;
-//                 Array.prototype.forEach.call(newnodes,function (newN){
-//                     console.log("Added a new node",newN.tagName);
-//                 } )
-//             })
-// }
-            
